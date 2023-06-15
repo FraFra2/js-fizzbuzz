f2c73895f195bd4bb713fb7d0ae57857787937b7
FraFra2 <kekkino41@gmail.com>
2023-06-15 22:52:24 +0200

    Bonus 1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
+let myDiv = document.getElementById(".container");
+
+
+
 for (let index = 0; index < 100; index++) {
+    let myLabel = document.createElement("h6    ");
+    myDiv.append(myLabel);
+
     if(index % 5 == 0 && index % 3 == 0){
-        console.log("fizz");
+        myLabel.innerHTML = ("fizz");
     }
     else if(index % 5 == 0){
-        console.log("buzz");
+        myLabel.innerHTML = ("buzz");
     }
     else if(index % 3 == 0){
-        console.log("buzz-fizz");
+        myLabel.innerHTML = ("fizz-buzz");
     }
     else{
         console.log(index);
